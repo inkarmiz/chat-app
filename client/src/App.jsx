4cabcf828ee54e697ba790f12ebf7f76e51e2121
inkarmiz <111295685+inkarmiz@users.noreply.github.com>
2025-10-14 11:27:53 +0300

    Auth UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Auth from "./pages/auth";
 import Chat from "./pages/chat";
