4718c8bc3f4dc25f4c77ea5863986f078df16f7c
inkarmiz <111295685+inkarmiz@users.noreply.github.com>
2025-10-14 22:02:49 +0300

    Add JWT token verification and middleware

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -7,9 +7,11 @@ import { toast } from "sonner";
 import { apiClient } from "@/lib/api-client";
 import { SIGNUP_ROUTE, LOGIN_ROUTE } from "@/utils/constants";
 import { useNavigate } from "react-router-dom";
+import { useAppStore } from "@/store";
 
 const Auth = () => {
   const navigate = useNavigate();
+  const { setUserInfo } = useAppStore();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -54,6 +56,7 @@ const Auth = () => {
       );
       // Check profile setup
       if (response.data.user.id) {
+        setUserInfo(response.data.user);
         if (response.data.user.profileSetup) navigate("/chat");
         else navigate("/profile");
       }
@@ -70,7 +73,7 @@ const Auth = () => {
         { withCredentials: true }
       );
       if (response.status === 201) {
-        toast.success("Account created successfully!");
+        setUserInfo(response.data.user);
         navigate("/profile");
       }
       console.log({ response });
--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,4 +1,18 @@
+import { useAppStore } from "@/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+// If profile is not setup, redirect to /profile, else show chat
 const Chat = () => {
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!userInfo.profileSetup) {
+      toast("Please complete your profile first.");
+      navigate("/profile");
+    }
+  }, [userInfo, navigate]);
   return <div>Chat</div>;
 };
 export default Chat;
