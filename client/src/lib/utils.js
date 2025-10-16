a6f2ced1f2f14e25ae5f20cf9b828f8c428ed832
inkarmiz <111295685+inkarmiz@users.noreply.github.com>
2025-10-16 14:23:57 +0300

    Profile UI and API

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const colors = [
+  "bg-[#712c4a57] text-[#ff006e] border-[1px] border-[#ff006faa]",
+  "bg-[#4a2c7173] text-[#7b2ff7] border-[1px] border-[#7b2ff7aa]",
+  "bg-[#2c3d71b8] text-[#006eff] border-[1px] border-[#006effaa]",
+  "bg-[#2c71734d] text-[#00ffe0] border-[1px] border-[#00ffe0aa]",
+]
+
+export const getColor = (color) => {
+  if (color >= 0 && color < colors.length) {
+    return colors[color];
+  }
+  return colors[0];
+}
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -97,3 +97,37 @@ export const getUserInfo = async (request, response, next) => {
         return response.status(500).send("Internal Server Error");
     }
 };
+
+// Update profile controller
+export const updateProfile = async (request, response, next) => {
+    try {
+        const { userId } = request;
+        const { firstName, lastName, color } = request.body;
+        if (!firstName || !lastName || color === undefined) {
+            return response.status(400).send("First name, last name and color are required.");
+        }
+        const userData = await User.findByIdAndUpdate(
+            userId,
+            {
+                firstName,
+                lastName,
+                color,
+                profileSetup: true
+            },
+            { new: true, runValidators: true }
+        );
+
+        return response.status(200).json({
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color,
+        });
+    } catch (error) {
+        console.log({ error });
+        return response.status(500).send("Internal Server Error");
+    }
+};
