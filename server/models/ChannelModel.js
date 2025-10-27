e3eae7a130f9d6b4b7f1fad683c9daa4c7dd60b1
inkarmiz <111295685+inkarmiz@users.noreply.github.com>
2025-10-27 17:09:41 +0200

    Add channel model and create channel API and get channel users

---
+++ server/models/ChannelModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const channelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    members: [{ type: mongoose.Schema.ObjectId, ref: "Users", required: true }],
+    admin: { type: mongoose.Schema.ObjectId, ref: "Users", required: true },
+    messages: [{ type: mongoose.Schema.ObjectId, ref: "Messages", required: false }],
+    createAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now(),
+    }, // Which channel has the last message, this is used to order channels
+});
+
+channelSchema.pre("save", function (next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+channelSchema.pre("findOneAndUpdate", function (next) {
+    this.set = ({ updatedAt: Date.now() });
+    next();
+});
+
+const Channel = mongoose.model("Channels", channelSchema);
+export default Channel;
